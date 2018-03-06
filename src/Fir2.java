2c17b3aa962cd8224991484b8b2f950449c08812
Utilisateur <Utilisateur@BNPPF006>
2018-03-06 09:36:35 +0100

    sav

--- src/Fir2.java
+++ src/Fir2.java
@@ -5,58 +5,62 @@ public class Fir2 {
     
     public static void main(String[] args) {
         int height;
+        String[] fir;
         
         System.out.println("Input height :");
         height= input.nextInt();
         if (height < 5 || height > 25) {
             System.out.println("Height must be between 5 and 25, try again!");
         } else {
-            String[] fir= new String[height+1];
+        	fir= new String[height+height/2];
             fir= makeFir2(height);
-            
+            // display fir
+            for (int i=0; i<height+height/2; i++) {
+            	System.out.println(fir[i]);
+            }
         }
              
     }
     
-    private static int length(int pNb) {
-        int rLength;
-        
-        rLength= 2 * pNb - 1;
-        
-        return rLength;
+    private static int length(int pNum) {
+    	// length of fir lines
+    	return (2 * pNum - 1);
     }
     
     private static String[] makeFir2(int pHeight) {
         int ct;
-        String[] rFir= new String[pHeight+1];
+        String[] rFir= new String[pHeight+pHeight/2];
         
         
-        for (ct = 1 ; ct <= pHeight ; ct += 1) {
-            makeLine2(length(pHeight), ct);
+        for (ct = 0 ; ct < pHeight ; ct++) {
+            rFir[ct]= makeLine2(length(pHeight), ct+1);
         }
         
-        for (ct = 1 ; ct <= pHeight/2 ; ct += 1) {
-            makeLine2(length(pHeight), 2);
+        for (ct = pHeight ; ct < pHeight+pHeight/2 ; ct++) {
+        	rFir[ct]= makeLine2(length(pHeight), 2);
         }
         
         return rFir;
     }
     
-    private static void makeLine2(int pLength, int pNbLine) {
-        int ct, nbSpace, nbStar;
+    private static String makeLine2(int pLength, int pNumLine) {
+        // pLength = 
+    	// pNumLine = 
+    	
+    	int ct, nbSpace, nbStar;
         String outputString, stringSpace, stringStar;
         
-        nbStar= length(pNbLine);
-        nbSpace= pLength - nbStar;
+        nbStar= length(pNumLine);
+        nbSpace= (pLength - nbStar) / 2;
         stringStar= "";
         stringSpace= "";
-        for (ct= 1 ; ct <= nbSpace / 2 ; ct+= 1) {
+        for (ct= 1 ; ct <= nbSpace ; ct++) {
             stringSpace= stringSpace + " ";
         }
-        for (ct= 1 ; ct <= nbStar ; ct+= 1) {
+        for (ct= 1 ; ct <= nbStar ; ct++) {
             stringStar = stringStar + "*";
         }
         outputString = stringSpace + stringStar;
-        System.out.println(outputString);
+        return outputString;
     }
 }
